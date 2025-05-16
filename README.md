a51b9b84ec9fb99f576e41926a29543336f1e254
anhi1 <anhiemolina@gmail.com>
2025-05-16 18:35:54 +0200

    cambios

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ npm install react-router-dom
 npm install bootstrap
   npm run dev
 
-  npm run build
+  
+DESPLEGAR EN VERCEL
+npm run build
+Crear un archivo json
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Si tu app está en la raíz del dominio, no necesitas base.
-  // base: '/', 
+ 
 })
